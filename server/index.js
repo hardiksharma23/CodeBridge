5b9f9696a70826007d70086e6bd11e46770a72f4
hardiksharma23 <sharmahardik270@gmail.com>
2025-07-10 17:16:29 +0530

    change the smtp mail

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-//database connect
 database.connect();
 
 //middlewares
@@ -41,7 +40,6 @@ app.use(
     })
 );
 
-//cloudinary connection
 cloudinaryConnect();
 
 //routes
